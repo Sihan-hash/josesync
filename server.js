2b427874508e012b35850394812a665efbb8a32c
Sihan-hash <2962torres@ieyp.edu.mx>
2026-05-31 17:15:19 -0600

    Update server.js

--- server.js
+++ server.js
@@ -10,9 +10,14 @@ wss.on('connection', (ws) => {
 
     console.log('Cliente conectado');
 
-    ws.on('message', (message) => {
+  ws.on('message', (message) => {
 
-        wss.clients.forEach((client) => {
+    console.log('====================');
+    console.log('MENSAJE RECIBIDO:');
+    console.log(message.toString());
+    console.log('====================');
+
+    wss.clients.forEach((client) => {
 
             if (
                 client.readyState === WebSocket.OPEN &&
